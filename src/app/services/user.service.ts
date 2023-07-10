3e3a2e18d0dbbd669c485c1cc40bc7fbc341bf2b
siddhraj-sinh <skrathod282001@gmail.com>
2023-07-10 16:18:02 +0530

    added loginUser method

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -8,13 +8,36 @@ import { Observable,map } from 'rxjs';
 export class UserService {
 
   url = "https://localhost:7024/api/User";
-
+  private currentUserKey = 'currentUser';
   constructor(private http:HttpClient) { }
 
   addUser(user:any):Observable<any>{
     return this.http.post(this.url,user);
    }
 
+
+   loginUser(email:string,password:string):Observable<boolean>{
+    interface User {
+      id:number,
+      email: string,
+      password: string,
+      firstName: string,
+      lastName: string,
+      organization: string,
+      contactDetails: string,
+    }
+    return this.http.get<User[]>(this.url).pipe(
+      map((users: User[]) => {
+        const user = users.find(u => u.email === email && u.password === password);
+        if (user) {
+          localStorage.setItem(this.currentUserKey, user.id.toString()); // Store user ID in local storage
+          return true;
+        } else {
+          return false;
+        }
+      })
+    );
+   }
    getAllUser():Observable<any[]>{
     return this.http.get<any>(this.url);
    }
