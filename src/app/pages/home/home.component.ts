897204e1203fb59590dc2c5b3c1bc1bf10784c0c
Aswathy1638 <aswathy1638@gmail.com>
2023-07-11 12:34:16 +0530

    added sidebar in home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  research: string[] = ['Research 1', 'Research 2', 'Research 3'];
 
+  openAddResearchModal() {
+    // Here, you can open a modal or perform any other action to add a new project.
+    // For simplicity, we will just log a message to the console.
+    console.log('Add new Research');
+
+}
 }
